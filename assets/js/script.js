b04a9520da241b9ee7e0a0f5bafb258252b0a955
Aldiansyah Fandevtian <fandevtian@gmail.com>
2022-08-24 22:54:09 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+var screenWidth = window.screen.width;
+var desktopScreen = 992;
+var mobileScreen = 768;
+
+var sectionEls = document.querySelectorAll(".section");
+var navBtn = document.getElementById("nav-btn");
+var menuBtn = document.getElementById("menu-btn");
+var navMenu = document.getElementById("nav-menu");
+
+// Toggle mobile menu
+function showMenu() {
+  navMenu.classList.toggle("active");
+}
+
+function menuEventListener() {
+  if (screenWidth < mobileScreen) {
+    navBtn.addEventListener("click", showMenu);
+
+    if (menuBtn) {
+      menuBtn.addEventListener("click", showMenu);
+    }
+  }
+}
+
+// Scroll Animation
+function sectionAnimate() {
+    const triggerBottom = (window.innerHeight / 6) * 4;
+
+    sectionEls.forEach((sectionEl) => {
+      const sectionTop = sectionEl.getBoundingClientRect().top;
+
+      if (sectionTop < triggerBottom) {
+        console.log(triggerBottom);
+        console.log("section top", sectionTop);
+        sectionEl.classList.add("show");
+      } else {
+        sectionEl.classList.remove("show");
+      }
+    });
+}
+
+function onSectionScroll() {
+    console.log(screenWidth, desktopScreen)
+  if (screenWidth > desktopScreen) {
+    window.addEventListener("scroll", sectionAnimate);
+  }
+}
+
+// Execute function
+onSectionScroll();
+menuEventListener();
